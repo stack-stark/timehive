8ac914f560b59552e42f3dc7bc4ebea2e4485b92
tmh <tostack@163.com>
2025-04-17 13:33:23 +0800

    feat: 更新版本号至1.0.0并优化提醒功能
    - 将版本号从0.0.1更新至1.0.0，标志着正式发布
    - 在提醒列表中添加新的提醒“准备下班吃饭”
    - 移除调试提醒，优化提醒逻辑，仅在工作时间触发
    - 添加LICENSE.txt文件，明确软件许可协议

--- src/extension.ts
+++ src/extension.ts
@@ -4,17 +4,17 @@ import { ReminderTreeDataProvider } from "./treeViewProvider";
 
 export const reminders = [
   { time: "30 8-17 * * 1-5", message: "🍵 喝水时间到，请注意补充水分！", remark: "周一到周五每30分钟" },
-  { time: "25 9 * * 1-5", message: "💰️ 股市即将开盘，请做好准备", remark: "周一到周五9:25" },
+  { time: "25 9 * * 1-5", message: "💴 股市即将开盘，请做好准备", remark: "周一到周五9:25" },
+  { time: "55 11 * * 1-5", message: "🍜 准备下班吃饭", remark: "周一到周五9:25" },
   { time: "45 14 * * 1-5", message: "💰️ 距离收盘还有15分钟，请注意操作", remark: "周一到周五14:45" },
   { time: "0 18 * * 1-5", message: "👨‍💻 下班时间到，请检查今日工作进度", remark: "周一到周五18:00" },
   { time: "0 10 * * 5", message: "🎮️ 记得领取本周Epic免费游戏！", remark: "周五10:00"},
-  { time: "* * * * *", message: "这是调试提醒", debug: true, remark: "每1分钟" },
 ];
 
 export function activate(context: vscode.ExtensionContext) {
   const jobs = reminders.map((reminder) => {
     return cron.schedule(reminder.time, () => {
-      if (reminder.debug || isWorkingHours()) {
+      if (isWorkingHours()) {
         vscode.window.showInformationMessage(reminder.message);
       }
     });
